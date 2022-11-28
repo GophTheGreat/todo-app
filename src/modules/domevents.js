8f1f128ff4bc111df81376e570d9154fc1f9d50e
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2022-11-28 09:23:09 -1000

    Update domevents.js
    showProjectForm start

--- src/modules/domevents.js
+++ src/modules/domevents.js
@@ -6,14 +6,14 @@ console.log('hi');
 export function addToSidebar(project){
   //create divs for all project pieces
   let projectDiv = document.createElement("div")
-  projectDiv.id = project.title;
+  projectDiv.id = project._title;
 
   let title = document.createElement("div")
-  title.innerHTML = project.title;
+  title.innerHTML = project._title;
   projectDiv.appendChild(title);
 
   let priority = document.createElement("div")
-  priority.innerHTML = project.priority;
+  priority.innerHTML = project._priority;
   projectDiv.appendChild(priority);
 
   let navBar = document.getElementById("navbar")
@@ -25,7 +25,21 @@ export const DOM_INITIALIZE = () => {
   let button = document.getElementById("addProject");
   console.log(button);
   button.addEventListener("click", () => {
+    showProjectForm();
     makeProject('title', 'priority');
     console.log("did thing");
   });
+}
+
+function showProjectForm(){
+  let form = document.createElement("form");
+  
+  let header = document.craeateElement("h2");
+  header.innerHTML = "New Project";
+  
+  let titleLabel = document.createElement("label")
+  titleLabel.innerHTML = "Title"
+  titleLabel.name = "title"
+  let title = document.createElement("input");
+  input.type = "text";
 }
